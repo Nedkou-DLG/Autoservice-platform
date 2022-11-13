6419688c46c1129b6dcf2af20a26b1e6d15dbe50
Nedyalko Boydev <nedyalko.boydev@gmail.com>
2022-11-13 17:22:51 +0200

    initial

---
+++ src/main/java/com/nedyalkoboydev/autoserviceplatform/AutoServicePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.nedyalkoboydev.autoserviceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServicePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoServicePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nedyalkoboydev/autoserviceplatform/AutoServicePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nedyalkoboydev.autoserviceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServicePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
